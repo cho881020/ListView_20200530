b6ed446d45aad97eced1bda9f0171ad64aa1e8c4
cho881020 <kj_cho@idealidea.co.kr>
2020-05-31 10:31:29 +0900

    받아낸 학생정보 활용 데이터 출력

--- app/src/main/java/kr/co/tjoeun/listview_20200530/ViewStudentActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200530/ViewStudentActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.listview_20200530
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_student.*
 import kr.co.tjoeun.listview_20200530.datas.Student
 
 class ViewStudentActivity : AppCompatActivity() {
@@ -12,6 +13,15 @@ class ViewStudentActivity : AppCompatActivity() {
 
         val student = intent.getSerializableExtra("student") as Student
 
+        nameTxt.text = student.name
+        ageTxt.text = "${student.getKoreanAge(2020)}세(${student.birthYear}년생)"
+
+        if (student.isMale) {
+            genderTxt.text = "남성"
+        }
+        else {
+            genderTxt.text = "여성"
+        }
 
 
 
